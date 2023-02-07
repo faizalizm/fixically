486e4f66b736d24f93ce75c4898271ba34ede3ad
Faizal <mfaizalismail@protonmail.com>
2023-02-08 04:49:09 +0800

    added controller and routes setup

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getGoal,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require('../controllers/goalController');
+
+router.route('/').get(getGoal).post(setGoal);
+router.route('/:id').delete(deleteGoal).put(updateGoal);
+
+module.exports = router;
